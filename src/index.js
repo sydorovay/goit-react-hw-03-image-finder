f7ba278532f44ed1f3de6dc2a422f5fdb823a881
Artem Sydorov <sydorovay@gmail.com>
2023-06-18 22:48:53 +0200

    delete wrong pass

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-import Searchbar from './components/searchbar';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 		<App />
