1ec9ba557c92ae99c5f50738fd79769d3c88179b
Artem Sydorov <sydorovay@gmail.com>
2023-06-18 22:47:58 +0200

    add searchbar+ style

---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = event => {
+    this.setState({ query: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    const { query } = this.state;
+
+    return (
+      <header className={styles.Searchbar}>
+        <form onSubmit={this.handleSubmit} className={styles.SearchForm}>
+          <button type="submit" className={styles['SearchForm-button']}>
+            <span className={styles['SearchForm-button-label']}>Search</span>
+          </button>
+
+          <input
+            className={styles['SearchForm-input']}
+            type="text"
+            value={query}
+            onChange={this.handleChange}
+            placeholder="Search images..."
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default Searchbar;
